640462fa6b006e854f162de91b0feaf36c7b5ecf
bernathmark <nitro_5_bernath_mark@outlook.hu>
2023-02-21 19:29:57 +0100

    routing

--- angular-in-nutshell/src/app/app.module.ts
+++ angular-in-nutshell/src/app/app.module.ts
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { ProductsComponent } from './products.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsService } from './products.service';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.modules';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, //importing for [(ngModel)]
+    AppRoutingModule
+  
   ],
   providers: [ProductsService], //here must importing the services
   bootstrap: [AppComponent]
